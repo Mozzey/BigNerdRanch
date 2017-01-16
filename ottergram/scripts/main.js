97b6c2ed3d4f752ebcd366abc688c07aa2e775a1
Mozzey <mbcacciano@gmail.com>
2017-01-16 13:57:45 -0500

    first few functions for gallery

---
+++ ottergram/scripts/main.js
@@ -0,0 +1,28 @@
+// These could also be const's //
+var DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
+var DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
+var THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
+
+
+function setDetails(imageUrl, titleText) {
+  'use strict';
+  var detailImage = document.querySelector(DETAIL_IMAGE_SELECTOR);
+  var detailTitle = document.querySelector(DETAIL_TITLE_SELECTOR);
+  detailImage.setAttribute('src', imageUrl);
+  detailTitle.textContent = titleText;
+}
+
+function imageFromThumb(thumbnail) {
+  'use stict';
+  return thumbnail.getAttribute('data-image-url');
+}
+
+function titleFromThumb(thumbnail) {
+  'use strcit';
+  return thumbnail.getAttribute('data-image-title');
+}
+
+function setDetailsFromThumb(thumbnail) {
+  'use strict';
+  setDetails(imageFromThumb(thumbnail), titleFromThumb(thumbnail));
+}
